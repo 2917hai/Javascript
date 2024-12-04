596c5a86c2ed05b57385f2b65d6d4b2db60e6fd3
Umesh Yadav <149967047+2917hai@users.noreply.github.com>
2024-12-04 09:36:20 +0530

    Add files via upload
    question in js

---
+++ question.js
@@ -0,0 +1,19 @@
+let para=document.querySelector("#im1");
+
+let para1=document.querySelector("#box1");
+para1.addEventListener("click",function()
+{
+    para.src='main_image1.webp';
+})
+
+let para2=document.querySelector("#box2");
+para2.addEventListener("click",function()
+{
+    para.src='main_image2.webp';
+})
+
+let para3=document.querySelector("#box3");
+para3.addEventListener("click",function()
+{
+    para.src='main_image3.webp';
+})
